b8ee483b376f210ea36833b4feac5c7f6f1b7941
ahmedmurtaza2022skipq <ahmed.murtaza.skipq@gmail.com>
2023-08-03 12:43:44 +0500

    Initial commit

---
+++ test/sprint2.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Sprint2 = require('../lib/sprint2-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sprint2-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Sprint2.Sprint2Stack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
